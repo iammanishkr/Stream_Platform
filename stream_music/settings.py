c4ab07e6de87b8d6d5a5d3db44b8885e133b3fda
Manish Kumar <112599556+iammanishkr@users.noreply.github.com>
2025-11-11 11:34:34 +0530

    Reorder WhiteNoiseMiddleware in MIDDLEWARE
    Moved WhiteNoiseMiddleware up in the MIDDLEWARE list.

--- stream_music/settings.py
+++ stream_music/settings.py
@@ -35,14 +35,15 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',   # move up
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
 
 ROOT_URLCONF = 'stream_music.urls'
 
